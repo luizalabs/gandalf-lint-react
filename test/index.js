d1bdddc06bbacaf0ae1fb50c39018cb890f80412
pablo.silva <pablo.silva@doclerholding.com>
2018-10-14 12:12:44 +0200

    created tests tha should fail on eslint check

---
+++ test/index.js
@@ -0,0 +1,15 @@
+import vaca from 'vaca'
+import React from 'react'
+
+class A extends React.PureComponent {
+    render() {
+        return "hello"
+    }
+}
+
+function one() { return 1 }
+
+function b() { return 2 }
+
+export default A
+
