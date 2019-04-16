c20e67cdef2c1411436991032ed660d804181320
Lucas <lucas@vilaboim.com>
2019-04-16 16:54:07 -0300

    feat(hooks): add react hooks rules

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = {
-  extends: ["@softboxlab/eslint-config-gandalf-lint", "./rules/gandalf"].map(
-    require.resolve
-  ),
-  plugins: ["eslint-plugin-react"],
+  extends: [
+    "@softboxlab/eslint-config-gandalf-lint",
+    './rules/gandalf'
+  ].map(require.resolve),
+  plugins: [
+    "eslint-plugin-react",
+    "react-hooks"
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
